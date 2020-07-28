676c43d19da61b7bc6015378e3f3e00477816b49
Aditya Kale <adityakale3@gmail.com>
2020-07-28 19:54:38 +0530

    Semi working Platform

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ app.on('ready', () => {
   // create main browser window
   win = new BrowserWindow({
       titleBarStyle: 'hidden',
-      width: 1920,
-      height: 1080,
+      width: 800,
+      height: 600,
       show: false, // don't show the main window
      webPreferences : {
          nodeIntegration : true
@@ -18,8 +18,7 @@ app.on('ready', () => {
   // create a new `splash`-Window 
   splash = new BrowserWindow({width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true});
   splash.loadURL(`file://${__dirname}/splash/splash.html`);
-  win.loadURL(`file://${__dirname}/main/index.html`);
-
+  win.loadURL(`file://${__dirname}/main/home.html`);
   // if main window is ready to show, then destroy the splash window and show up the main window
   win.once('ready-to-show', () => {
       setTimeout(() => {
